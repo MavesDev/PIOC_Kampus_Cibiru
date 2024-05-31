708c6816b25003fb09b7ce3660937f7b05222bb8
Alfeus Martinus Sinurat <feusmartinus@gmail.com>
2024-05-31 21:46:09 +0700

    pagination

--- template/postingan.php
+++ template/postingan.php
@@ -6,16 +6,16 @@
         if (isset($_SESSION['status']) && $_SESSION['status'] != '') {
             if ($_SESSION['status'] === '<b>Data Berhasil Masuk</b>, Mohon Menunggu Persetujuan Admin') {
         ?>
-        <div class="alert alert-success" role="alert">
-            <?php echo $_SESSION['status'] ?>
-        </div>
-        <?php
+                <div class="alert alert-success" role="alert">
+                    <?php echo $_SESSION['status'] ?>
+                </div>
+            <?php
             } else {
             ?>
-        <div class="alert alert-danger" role="alert">
-            <?php echo $_SESSION['status'] ?>
-        </div>
-        <?php
+                <div class="alert alert-danger" role="alert">
+                    <?php echo $_SESSION['status'] ?>
+                </div>
+            <?php
             }
             ?>
         <?php
@@ -43,32 +43,70 @@
             <?php
             $connection = mysqli_connect("localhost", "root", "", "pioc");
 
-            $fetch_image_query = "SELECT * FROM postingan ";
+            $jumlahDataPerHalaman = 2;
+            $result = mysqli_query($connection, "SELECT * FROM postingan");
+            $jumlahData = mysqli_num_rows($result);
+            $jumlahHalaman = ceil($jumlahData / $jumlahDataPerHalaman);
+            // var_dump($jumlahHalaman);
+
+            $activePage = (isset($_GET['page'])) ? $_GET['page'] : 1;
+
+            $awalData = ($jumlahDataPerHalaman * $activePage) - $jumlahDataPerHalaman;
+
+            $fetch_image_query = "SELECT * FROM postingan LIMIT $awalData, $jumlahDataPerHalaman";
             $fetch_image_query_run = mysqli_query($connection, $fetch_image_query);
 
             ?>
 
             <?php if (mysqli_num_rows($fetch_image_query_run) > 0) : ?>
-            <?php foreach ($fetch_image_query_run as $data) : ?>
-            <div class="col-lg-4">
-                <div class="services-box p-4 mt-4">
-                    <img src="imgUpload/<?= $data['gambar']; ?>" class="img-thumbnail mx-auto d-block"
-                        alt="random-image">
-                    <h5 class="mt-4"><?= $data['nama_kegiatan']; ?></h5>
-                    <p class="text-muted mt-3"><?= $data['deskripsi']; ?></p>
-                    <div class="mt-3">
-                        <a href="" class="text-primary f-4" type="button" class="btn btn-primary" data-bs-toggle="modal"
-                            data-bs-target="#exampleModal">
-                            <i class="mdi mdi-heart"></i>
-                            <i class="mdi mdi-comment ms-2"></i>
-                        </a>
+                <?php foreach ($fetch_image_query_run as $data) : ?>
+                    <div class="col-lg-4">
+                        <div class="services-box p-4 mt-4">
+                            <img src="imgUpload/<?= $data['gambar']; ?>" class="img-thumbnail mx-auto d-block" alt="random-image">
+                            <h5 class="mt-4"><?= $data['nama_kegiatan']; ?></h5>
+                            <p class="text-muted mt-3"><?= $data['deskripsi']; ?></p>
+                            <div class="mt-3">
+                                <a href="" class="text-primary f-4" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                    <i class="mdi mdi-heart"></i>
+                                    <i class="mdi mdi-comment ms-2"></i>
+                                </a>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <?php endforeach; ?>
+                <?php endforeach; ?>
             <?php endif; ?>
 
         </div>
+        <nav aria-label="Page navigation example" class="mt-5">
+            <ul class="pagination justify-content-center">
+                <?php if ($activePage > 1) : ?>
+                    <li class="page-item">
+                        <a class="page-link" href="?page=<?= $activePage - 1; ?>">Previous</a>
+                    </li>
+                <?php else : ?>
+                    <li class="page-item disabled">
+                        <a class="page-link"">Previous</a>
+                    </li>    
+                <?php endif; ?>
+                <?php for ($i = 1; $i <= $jumlahHalaman; $i++) : ?>
+                    <?php if ($i == $activePage) : ?>
+                        <li class=" page-item"><a class="page-link active" href="?page=<?= $i; ?>"><?= $i ?></a>
+                    </li>
+                <?php else : ?>
+                    <li class="page-item"><a class="page-link" href="?page=<?= $i; ?>"><?= $i ?></a></li>
+                <?php endif; ?>
+            <?php endfor; ?>
+            <?php if ($activePage < $jumlahHalaman) : ?>
+                <li class="page-item">
+                    <a class="page-link" href="?page=<?= $activePage + 1; ?>">Next</a>
+                </li>
+            <?php else : ?>
+                <li class="page-item disabled">
+                    <a class="page-link" href="#">Next</a>
+                </li>
+            <?php endif; ?>
+            </ul>
+        </nav>
     </div>
 </section>
 
@@ -111,9 +149,7 @@
                                             </p>
                                         </div>
                                         <div class="d-flex">
-                                            <a href="#" class="text-secondary me-2"><i
-                                                    class="mdi mdi-reply me-1"></i><small
-                                                    class="text-muted me-1">Reply</small></a>
+                                            <a href="#" class="text-secondary me-2"><i class="mdi mdi-reply me-1"></i><small class="text-muted me-1">Reply</small></a>
                                             <p style="margin-bottom: 0;"><small class="text-muted">Last updated 3 mins
                                                     ago</small></p>
                                         </div>
@@ -123,8 +159,7 @@
                             <a href="#" class="text-secondary me-2"><small class="text-muted me-1">Sembunyikan
                                     balasan</small></a>
                             <div class="d-flex">
-                                <a href="#" class="text-secondary me-2"><i class="mdi mdi-reply me-1"></i><small
-                                        class="text-muted me-1">Reply</small></a>
+                                <a href="#" class="text-secondary me-2"><i class="mdi mdi-reply me-1"></i><small class="text-muted me-1">Reply</small></a>
                                 <p><small class="text-muted me-3">Last updated 3 mins ago</small></p>
                             </div>
                         </div>
