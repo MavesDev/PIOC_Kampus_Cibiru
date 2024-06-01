be544d8f13f8e41afe0cd622e97b673959924bad
Alfeus Martinus Sinurat <feusmartinus@gmail.com>
2024-06-01 16:34:37 +0700

    config jquery & ajax untuk push notifikasi

---
+++ js/notif.js
@@ -0,0 +1,92 @@
+$(document).ready(function () {
+    showNotifications();
+    setInterval(showNotifications, 5000);
+
+    $('#notifications').hide();
+
+    $('#nf-btn').click(function(){
+        $('#notifications').toggle();
+        seenNotification();
+    });
+
+    $('#nf-btn').click(function(e){
+        e.stopPropagation();
+    })
+
+    $('html').click(function(){
+        $('#notifications').hide();
+    });
+
+});
+
+function showNotifications() {
+    $.ajax({
+        type: "POST",
+        url: "php/fetch.php",
+        data: {
+            key: '123'
+        },
+        cache: false,
+        success: function (data) {
+            const contentData = JSON.parse(data);
+            $('#notifications').html('');
+            console.log(contentData);
+    
+            if (contentData.length > 0) {
+                $('#numHead').html(contentData[0].total);
+    
+                for (let i = 1; i < contentData.length; i++) {
+                    let date = contentData[i].date;
+                    let dilihat = contentData[i].dilihat;
+                    let id_notifikasi = contentData[i].id_notifikasi;
+                    let id_user = contentData[i].id_user;
+                    let notif_table = contentData[i].notif_table;
+                    let status = contentData[i].status;
+                    let type = contentData[i].type;
+                    let uniqueid = contentData[i].uniqueid;
+                    let url = contentData[i].url;
+    
+                    if (type == 'contact') {
+                        type = "Halo! ada pesan baru";
+                    }
+    
+                    if (dilihat == 'unseen') {
+                        dilihat = 'success';
+                    } else {
+                        dilihat = 'dark';
+                    }
+    
+                    let resultDiv = `
+                        <a href="${url}?notification=${uniqueid}">
+                            <div class="alert alert-${dilihat}" role="alert" title="${date}">
+                                ${type}
+                            </div>
+                        </a>
+                    `;
+                    
+                    $('#notifications').append(resultDiv);
+                }
+            } else {
+                $('#notifications').html("<p>No Notification</p>");
+            }
+        },
+        error: function (xhr, status, error) {
+            console.error('AJAX error: ' + xhr, status, error);
+        }
+    });    
+}
+
+
+function seenNotification() {
+    $.ajax({
+        type: "POST",
+        url: "php/fetch.php",
+        data: {
+            key: '1234'
+        },
+        cache: false,
+        succes: function(data){
+
+        }
+    });
+}
