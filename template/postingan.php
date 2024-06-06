d8424f326e68bc890bac2ec7819506c49103289d
Alfeus Martinus Sinurat <feusmartinus@gmail.com>
2024-06-06 19:43:50 +0700

    tambah fitur searching, membungkus postingan dengan div lagi

--- template/postingan.php
+++ template/postingan.php
@@ -2,8 +2,8 @@
 
 include "php/code.php";
 
-if( isset($_GET["id"]) ) {
-    if(addLike($_GET) > 0) {
+if (isset($_GET["id"])) {
+    if (addLike($_GET) > 0) {
         echo "
             <script>
                 alert('berhasil ditambahkan!');
@@ -28,16 +28,16 @@ if( isset($_GET["id"]) ) {
         if (isset($_SESSION['status']) && $_SESSION['status'] != '') {
             if ($_SESSION['status'] === '<b>Data Berhasil Masuk</b>, Mohon Menunggu Persetujuan Admin') {
         ?>
-        <div class="alert alert-success" role="alert">
-            <?php echo $_SESSION['status'] ?>
-        </div>
-        <?php
+                <div class="alert alert-success" role="alert">
+                    <?php echo $_SESSION['status'] ?>
+                </div>
+            <?php
             } else {
             ?>
-        <div class="alert alert-danger" role="alert">
-            <?php echo $_SESSION['status'] ?>
-        </div>
-        <?php
+                <div class="alert alert-danger" role="alert">
+                    <?php echo $_SESSION['status'] ?>
+                </div>
+            <?php
             }
             ?>
         <?php
@@ -59,82 +59,88 @@ if( isset($_GET["id"]) ) {
 
         <?php include "upload.php" ?>
 
+
         <div class="row mt-5 pt-4">
-
-            <?php
-            $connection = mysqli_connect("localhost", "root", "", "pioc");
-
-            $jumlahDataPerHalaman = 2;
-            $result = mysqli_query($connection, "SELECT * FROM postingan");
-            $jumlahData = mysqli_num_rows($result);
-            $jumlahHalaman = ceil($jumlahData / $jumlahDataPerHalaman);
-            // var_dump($jumlahHalaman);
-
-            $activePage = (isset($_GET['page'])) ? $_GET['page'] : 1;
-
-            $awalData = ($jumlahDataPerHalaman * $activePage) - $jumlahDataPerHalaman;
-
-            $fetch_image_query = "SELECT * FROM postingan LIMIT $awalData, $jumlahDataPerHalaman";
-            $fetch_image_query_run = mysqli_query($connection, $fetch_image_query);
-
-            ?>
-
-            <?php if (mysqli_num_rows($fetch_image_query_run) > 0) : ?>
-            <?php foreach ($fetch_image_query_run as $data) : ?>
-            <div class="col-lg-4">
-                <div class="services-box p-4 mt-4">
-                    <img src="imgUpload/<?= $data['gambar']; ?>" class="img-thumbnail mx-auto d-block"
-                        alt="random-image">
-                    <h5 class="mt-4"><?= $data['nama_kegiatan']; ?></h5>
-                    <p class="text-muted mt-3"><?=   $data['deskripsi']; ?></p>
-                    <?php $idPost = $data['id_postingan'] ?>
-                    <div class="mt-3">
-                        <a href="post.php?id=<?= $_SESSION["id"] ?>&id_post=<?= $data["id_postingan"] ?>"
-                            class="text-primary f-4" class="btn btn-primary" name="like">
-                            <i class="mdi mdi-heart"></i>
-                            <?php $result = query("SELECT COUNT(id_suka) AS jumlahLike FROM `suka` WHERE id_postingan = $idPost"); ?>
-                            <small><?= $result[0]["jumlahLike"] ?></small>
-                        </a>
-                        <a href="comment.php?id_post=<?= $data["id_postingan"] ?>" class="text-primary f-4"
-                            class="btn btn-primary">
-                            <i class="mdi mdi-comment ms-2"></i>
-                            <?php $result = query("SELECT COUNT(id_komentar) AS jumlahKomentar FROM `komentar` WHERE id_postingan = $idPost"); ?>
-                            <small><?= $result[0]["jumlahKomentar"] ?></small>
-                        </a>
-                    </div>
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Cari Disini" aria-label="Recipient's username" aria-describedby="basic-addon2" id="keyword">
+                <div class="input-group-append">
+                    <button class="btn btn-outline-secondary" type="button" id="tombol-cari">Cari</button>
                 </div>
             </div>
-            <?php endforeach; ?>
-            <?php endif; ?>
+
+            <div class="row" id="container-post">
+                <?php
+                include './php/connection.php';
+
+                $jumlahDataPerHalaman = 2;
+                $result = mysqli_query($conn, "SELECT * FROM postingan");
+                $jumlahData = mysqli_num_rows($result);
+                $jumlahHalaman = ceil($jumlahData / $jumlahDataPerHalaman);
+                // var_dump($jumlahHalaman);
+
+                $activePage = (isset($_GET['page'])) ? $_GET['page'] : 1;
+
+                $awalData = ($jumlahDataPerHalaman * $activePage) - $jumlahDataPerHalaman;
+
+                $fetch_image_query = "SELECT * FROM postingan LIMIT $awalData, $jumlahDataPerHalaman";
+                $fetch_image_query_run = mysqli_query($connection, $fetch_image_query);
+
+                ?>
+
+                <?php if (mysqli_num_rows($fetch_image_query_run) > 0) : ?>
+                    <?php foreach ($fetch_image_query_run as $data) : ?>
+                        <div class="col-lg-4" id="container-post">
+                            <div class="services-box p-4 mt-4">
+                                <img src="imgUpload/<?= $data['gambar']; ?>" class="img-thumbnail mx-auto d-block" alt="random-image">
+                                <h5 class="mt-4"><?= $data['nama_kegiatan']; ?></h5>
+                                <p class="text-muted mt-3"><?= $data['deskripsi']; ?></p>
+                                <?php $idPost = $data['id_postingan'] ?>
+                                <div class="mt-3">
+                                    <a href="post.php?id=<?= $_SESSION["id"] ?>&id_post=<?= $data["id_postingan"] ?>" class="text-primary f-4" class="btn btn-primary" name="like">
+                                        <i class="mdi mdi-heart"></i>
+                                        <?php $result = query("SELECT COUNT(id_suka) AS jumlahLike FROM `suka` WHERE id_postingan = $idPost"); ?>
+                                        <small><?= $result[0]["jumlahLike"] ?></small>
+                                    </a>
+                                    <a href="comment.php?id_post=<?= $data["id_postingan"] ?>" class="text-primary f-4" class="btn btn-primary">
+                                        <i class="mdi mdi-comment ms-2"></i>
+                                        <?php $result = query("SELECT COUNT(id_komentar) AS jumlahKomentar FROM `komentar` WHERE id_postingan = $idPost"); ?>
+                                        <small><?= $result[0]["jumlahKomentar"] ?></small>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    <?php endforeach; ?>
+                <?php endif; ?>
+            </div>
 
         </div>
         <nav aria-label="Page navigation example" class="mt-5">
             <ul class="pagination justify-content-center">
                 <?php if ($activePage > 1) : ?>
-                <li class="page-item">
-                    <a class="page-link" href="?page=<?= $activePage - 1; ?>">Previous</a>
-                </li>
+                    <li class="page-item">
+                        <a class="page-link" href="?page=<?= $activePage - 1; ?>">Previous</a>
+                    </li>
                 <?php else : ?>
-                <li class="page-item disabled">
-                    <a class="page-link">Previous</a>
-                </li>
+                    <li class="page-item disabled">
+                        <a class="page-link">Previous</a>
+                    </li>
                 <?php endif; ?>
                 <?php for ($i = 1; $i <= $jumlahHalaman; $i++) : ?>
-                <?php if ($i == $activePage) : ?>
-                <li class=" page-item"><a class="page-link active" href="?page=<?= $i; ?>"><?= $i ?></a>
-                </li>
-                <?php else : ?>
-                <li class="page-item"><a class="page-link" href="?page=<?= $i; ?>"><?= $i ?></a></li>
-                <?php endif; ?>
+                    <?php if ($i == $activePage) : ?>
+                        <li class=" page-item"><a class="page-link active" href="?page=<?= $i; ?>"><?= $i ?></a>
+                        </li>
+                    <?php else : ?>
+                        <li class="page-item"><a class="page-link" href="?page=<?= $i; ?>"><?= $i ?></a></li>
+                    <?php endif; ?>
                 <?php endfor; ?>
                 <?php if ($activePage < $jumlahHalaman) : ?>
-                <li class="page-item">
-                    <a class="page-link" href="?page=<?= $activePage + 1; ?>">Next</a>
-                </li>
+                    <li class="page-item">
+                        <a class="page-link" href="?page=<?= $activePage + 1; ?>">Next</a>
+                    </li>
                 <?php else : ?>
-                <li class="page-item disabled">
-                    <a class="page-link" href="#">Next</a>
-                </li>
+                    <li class="page-item disabled">
+                        <a class="page-link" href="#">Next</a>
+                    </li>
                 <?php endif; ?>
             </ul>
         </nav>
@@ -180,9 +186,7 @@ if( isset($_GET["id"]) ) {
                                             </p>
                                         </div>
                                         <div class="d-flex">
-                                            <a href="#" id="2" class="text-secondary me-2" data-bs-toggle="modal"
-                                                data-bs-target="#exampleModal2"><i class="mdi mdi-reply me-1"></i><small
-                                                    class="text-muted me-1">Reply</small></a>
+                                            <a href="#" id="2" class="text-secondary me-2" data-bs-toggle="modal" data-bs-target="#exampleModal2"><i class="mdi mdi-reply me-1"></i><small class="text-muted me-1">Reply</small></a>
                                             <p style="margin-bottom: 0;"><small class="text-muted">Last updated 3 mins
                                                     ago</small></p>
                                         </div>
@@ -192,8 +196,7 @@ if( isset($_GET["id"]) ) {
                             <a href="#" class="text-secondary me-2"><small class="text-muted me-1">Sembunyikan
                                     balasan</small></a>
                             <div class="d-flex">
-                                <a href="#" class="text-secondary me-2"><i class="mdi mdi-reply me-1"></i><small
-                                        class="text-muted me-1">Reply</small></a>
+                                <a href="#" class="text-secondary me-2"><i class="mdi mdi-reply me-1"></i><small class="text-muted me-1">Reply</small></a>
                                 <p><small class="text-muted me-3">Last updated 3 mins ago</small></p>
                             </div>
                         </div>
@@ -227,9 +230,7 @@ if( isset($_GET["id"]) ) {
                                             </p>
                                         </div>
                                         <div class="d-flex">
-                                            <a href="#" class="text-secondary me-2"><i
-                                                    class="mdi mdi-reply me-1"></i><small
-                                                    class="text-muted me-1">Reply</small></a>
+                                            <a href="#" class="text-secondary me-2"><i class="mdi mdi-reply me-1"></i><small class="text-muted me-1">Reply</small></a>
                                             <p style="margin-bottom: 0;"><small class="text-muted">Last updated 3 mins
                                                     ago</small></p>
                                         </div>
@@ -239,8 +240,7 @@ if( isset($_GET["id"]) ) {
                             <a href="#" class="text-secondary me-2"><small class="text-muted me-1">Sembunyikan
                                     balasan</small></a>
                             <div class="d-flex">
-                                <a href="#" class="text-secondary me-2"><i class="mdi mdi-reply me-1"></i><small
-                                        class="text-muted me-1">Reply</small></a>
+                                <a href="#" class="text-secondary me-2"><i class="mdi mdi-reply me-1"></i><small class="text-muted me-1">Reply</small></a>
                                 <p><small class="text-muted me-3">Last updated 3 mins ago</small></p>
                             </div>
                         </div>
@@ -250,10 +250,8 @@ if( isset($_GET["id"]) ) {
             <form action="" method="POST" id="commentForm">
                 <div class="modal-footer">
                     <div class="input-group mb-3">
-                        <input type="text" class="form-control" id="inputAja" placeholder="Tulis Komentar"
-                            aria-label="Recipient's username" aria-describedby="commentSubmit" name="deskripsi">
-                        <button class=" btn btn-primary" type="submit" id="commentSubmit" name="addComment"
-                            form="commentForm">Kirim</button>
+                        <input type="text" class="form-control" id="inputAja" placeholder="Tulis Komentar" aria-label="Recipient's username" aria-describedby="commentSubmit" name="deskripsi">
+                        <button class=" btn btn-primary" type="submit" id="commentSubmit" name="addComment" form="commentForm">Kirim</button>
                     </div>
                 </div>
             </form>
