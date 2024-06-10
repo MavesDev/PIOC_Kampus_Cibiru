a8b11cad997ccbc18933f60b79c89188666e3987
MavesDev <littleudel@gmail.com>
2024-06-10 10:22:18 +0700

    Fix Navbar

--- template/user/navbar.php
+++ template/user/navbar.php
@@ -12,6 +12,7 @@
         <div class="collapse navbar-collapse" id="navbarCollapse">
             <ul class="navbar-nav ms-auto navbar-center" id="mySidenav">
                 <div class="nf-area">
+                    <?php if(isset($_SESSION["nama_user"])) : ?>
                     <button type="button" class="btn btn-primary position-relative p-2" id="nf-btn">
                         <i class="bi bi-bell"></i>
                         <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
@@ -19,6 +20,7 @@
                             99+
                             <!-- <span class="visually">unread messages</span> -->
                         </span>
+                        <?php endif ; ?>
                     </button>
                     <div class="rounded me-5" style="position: absolute; z-index: 9999; margin-left: -10rem">
                         <div class="nf-message border-success mt-3 shadow p-3 mb-5 bg-body-tertiary rounded w-100 p-3"
@@ -28,6 +30,7 @@
             </ul>
 
             <ul class="navbar-nav navbar-center">
+
                 <?php if(isset($_SESSION["nama_user"])) : ?>
                 <li class="nav-item">
                     <a class="nav-link" href="bookmarks.php">
@@ -35,20 +38,24 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <!-- ---------------------------------------------------------------------- page profile disini -->
                     <a href="profile.php" class="nav"><?php echo $_SESSION['nama_user']; ?></a>
                 </li>
-
-                <?php if(!isset($_SESSION["nama_user"])) ?>
-                <li class="nav-item d-inline-block d-lg-none">
-                    <a href="" class="nav-link">Sign Up</a>
+                <?php elseif(!isset($_SESSION["nama_user"])) : ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="registrasi.php">
+                        <i class="fas fa-bookmark"></i> Sign In
+                    </a>
                 </li>
-
                 <?php endif ; ?>
+
             </ul>
             <?php if(isset($_SESSION["nama_user"])) : ?>
             <div class="navbar-button d-none d-lg-inline-block">
-                <a href="login.php" class="btn btn-sm btn-soft-primary btn-round">Log out</a>
+                <a href="logout.php" class="btn btn-sm btn-soft-primary btn-round">Log out</a>
+            </div>
+            <?php elseif(!isset($_SESSION["nama_user"])) : ?>
+            <div class="navbar-button d-none d-lg-inline-block">
+                <a href="login.php" class="btn btn-sm btn-soft-primary btn-round">Log In</a>
             </div>
             <?php endif ; ?>
         </div>
